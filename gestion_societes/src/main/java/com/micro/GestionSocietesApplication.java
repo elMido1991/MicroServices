42b16183b4eafc69155b7a6e8f902a3f4c120c02
PC <PC@Mido-PC>
2019-01-05 08:49:00 +0000

    first commit

---
+++ gestion_societes/src/main/java/com/micro/GestionSocietesApplication.java
@@ -0,0 +1,14 @@
+package com.micro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSocietesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSocietesApplication.class, args);
+	}
+
+}
+
